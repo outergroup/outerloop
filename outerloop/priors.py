a3ae1cb9add1155d7fe86b7a161b7411a59fb05c
Marcus Lewis <git@probablymarcus.com>
2023-03-07 09:50:46 -0800

    fast DirichletAt prior

--- outerloop/priors.py
+++ outerloop/priors.py
@@ -41,15 +41,40 @@ class DirichletAtPrior(gpytorch.priors.Prior):
     def __init__(self, concentrations, validate_args=False, transform=None):
         super().__init__(validate_args=validate_args)
         self._transform = transform
+        self.concentrations = [torch.as_tensor(c) for c in concentrations]
         self.lengths = [len(c) for c in concentrations]
-        self.priors = torch.nn.ModuleList(
-            DirichletPrior(c)
-            for c in concentrations
+
+        concentration = torch.cat(self.concentrations)
+        self.concentration_minus_1 = concentration - 1.0
+
+        reduced_size = len(self.lengths)
+        reduce_indices = torch.arange(
+            reduced_size
+        ).repeat_interleave(torch.tensor(self.lengths))
+        component1 = torch.lgamma(
+            torch.zeros(reduced_size).index_add_(
+                -1, reduce_indices, concentration
+            )
         )
+        component2 = torch.zeros(reduced_size).index_add_(
+            -1, reduce_indices, torch.lgamma(concentration)
+        )
+        self.additive_constant = (component1 - component2).sum()
+
+    def _apply(self, fn):
+        self.concentrations = [fn(c) for c in self.concentrations]
+        self.concentration_minus_1 = fn(self.concentration_minus_1)
+        self.additive_constant = fn(self.additive_constant)
 
     def log_prob(self, x):
         with torch.profiler.record_function("DirichletAtPrior.log_prob"):
-            return torch.stack(
-                [prior.log_prob(x_)
-                for x_, prior in zip(x.split(self.lengths, -1), self.priors)]
-            ).sum()
+            return ((x.log() * self.concentration_minus_1).sum(-1)
+                    + self.additive_constant)
+
+    def rsample(self, sample_shape=()):
+        return torch.cat(
+            [DirichletPrior(c).rsample(sample_shape)
+             for c in self.concentrations],
+            -1
+        )
+
