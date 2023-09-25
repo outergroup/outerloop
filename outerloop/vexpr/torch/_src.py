fdb2b96dae5aa364f70a4b33bdb9e69e4e9233a6
Marcus Lewis <git@probablymarcus.com>
2023-09-25 16:40:41 -0700

    Profile matern

--- outerloop/vexpr/torch/_src.py
+++ outerloop/vexpr/torch/_src.py
@@ -15,10 +15,11 @@ from vexpr.torch.register_pushthrough import (
 matern_p, matern = vexpr.core._p_and_constructor("matern")
 
 def matern_impl(d, nu=2.5):
-    assert nu == 2.5
-    exp_component = torch.exp(-math.sqrt(5) * d)
-    constant_component = 1. + (math.sqrt(5) * d) + (5. / 3.) * d**2
-    return constant_component * exp_component
+    with torch.profiler.record_function("matern"):
+        assert nu == 2.5
+        exp_component = torch.exp(-math.sqrt(5) * d)
+        constant_component = 1. + (math.sqrt(5) * d) + (5. / 3.) * d**2
+        return constant_component * exp_component
 
 
 vexpr.core.eval_impls[matern_p] = matern_impl
